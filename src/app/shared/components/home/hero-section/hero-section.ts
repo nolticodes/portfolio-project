90ecd4a396e5ad10f2982687fb76485f88b4517e
Denis Nolting <denis.nolting@gmx.de>
2026-06-11 13:56:10 +0200

    feat: hover aniamtion "hallo Welt" -> "Ich bin ..."

--- src/app/shared/components/home/hero-section/hero-section.ts
+++ src/app/shared/components/home/hero-section/hero-section.ts
@@ -7,6 +7,37 @@ import { ChangeLetterHover } from '../../../directives/change-letter-hover';
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
-export class HeroSection {
 
+export class HeroSection {
+  displayText = 'Hallo Welt';
+
+  defaultText = 'Hallo Welt';
+  hoverText = 'Ich bin Denis Nolting';
+
+  private nextText = this.defaultText;
+
+  isTextFading = false;
+
+  onMouseEnter() {
+    this.changeHeroText(this.hoverText);
+  }
+
+  onMouseLeave() {
+    this.changeHeroText(this.defaultText);
+  }
+
+  changeHeroText(newText: string) {
+    if (newText === this.displayText) return;
+
+    this.nextText = newText;
+    this.isTextFading = true;
+  }
+
+  onTextTransitionEnd(event: TransitionEvent) {
+    if (event.propertyName !== 'opacity') return;
+    if (!this.isTextFading) return;
+
+    this.displayText = this.nextText;
+    this.isTextFading = false;
+  }
 }
