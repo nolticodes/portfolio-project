12995442f1b9ab752699d6eea4ab74b1b1c30184
Denis Nolting <denis.nolting@gmx.de>
2026-06-16 15:09:27 +0200

    design: responsive design skills component

--- src/app/shared/components/home/skills/skills.ts
+++ src/app/shared/components/home/skills/skills.ts
@@ -11,18 +11,18 @@ export class Skills {
   private cdr = inject(ChangeDetectorRef)
 
   frontendSkills = [
-    { name: "HTML", imgUrl: "./assets/icons/frontend_skills/HTML.png" },
-    { name: "CSS", imgUrl: "./assets/icons/frontend_skills/CSS.png" },
-    { name: "Git", imgUrl: "./assets/icons/frontend_skills/Git.png" },
-    { name: "JavaScript", imgUrl: "./assets/icons/frontend_skills/Js.png" },
-    { name: "REST-API", imgUrl: "./assets/icons/frontend_skills/Rest-Api.png" },
-    { name: "Angular", imgUrl: "./assets/icons/frontend_skills/Angular.png" },
-    { name: "TypeScript", imgUrl: "./assets/icons/frontend_skills/Ts.png" },
+    { name: "HTML", imgUrl: "./assets/icons/frontend_skills/HTML.svg" },
+    { name: "CSS", imgUrl: "./assets/icons/frontend_skills/CSS.svg" },
+    { name: "Git", imgUrl: "./assets/icons/frontend_skills/Git.svg" },
+    { name: "JavaScript", imgUrl: "./assets/icons/frontend_skills/Js.svg" },
+    { name: "REST-API", imgUrl: "./assets/icons/frontend_skills/Rest-Api.svg" },
+    { name: "Angular", imgUrl: "./assets/icons/frontend_skills/Angular.svg" },
+    { name: "TypeScript", imgUrl: "./assets/icons/frontend_skills/Ts.svg" },
   ];
 
   pullToPeelImages = [
-    './assets/stickers/skills/pull_to_peel/p2p_1.png',
-    './assets/stickers/skills/pull_to_peel/p2p_2.png',
+    './assets/stickers/skills/pull_to_peel/p2p_1.svg',
+    './assets/stickers/skills/pull_to_peel/p2p_2.svg',
     './assets/stickers/skills/pull_to_peel/p2p_3.png',
   ];
 
