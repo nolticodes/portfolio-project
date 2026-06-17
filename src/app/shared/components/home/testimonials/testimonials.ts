d77436873f9ba63095dbe15438f73e5d09fef93e
Denis Nolting <denis.nolting@gmx.de>
2026-06-17 18:38:59 +0200

    refactor and style: small changes in testimonials section

--- src/app/shared/components/home/testimonials/testimonials.ts
+++ src/app/shared/components/home/testimonials/testimonials.ts
@@ -9,27 +9,29 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class Testimonials {
   testimonials = [
-  {
-    cardImage: 'assets/img/testimonials/testimonial_a.svg',
-    hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
-    name: 'Weihnachtsmann',
-    commentKey: 'home.testimonials.comment_one',
-    rotationClass: 'rotate_left'
-  },
-  {
-    cardImage: 'assets/img/testimonials/testimonial_b.svg',
-    hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
-    name: 'Osterhase',
-    commentKey: 'home.testimonials.comment_two',
-    rotationClass: ''
-  },
-  {
-    cardImage: 'assets/img/testimonials/testimonial_a.svg',
-    hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
-    name: 'Zahnfee',
-    commentKey: 'home.testimonials.comment_three',
-    profileUrl: 'https://linkedin.com/in/...',
-    rotationClass: 'rotate_right'
-  }
-];
+    {
+      cardImage: 'assets/img/testimonials/testimonial_a.svg',
+      hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
+      name: 'Weihnachtsmann',
+      commentKey: 'home.testimonials.comment_one',
+      profileUrl: 'https://de.linkedin.com',
+      rotationClass: 'rotate_left'
+    },
+    {
+      cardImage: 'assets/img/testimonials/testimonial_b.svg',
+      hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
+      name: 'Weihnachtsmann',
+      commentKey: 'home.testimonials.comment_two',
+      profileUrl: 'https://de.linkedin.com',
+      rotationClass: ''
+    },
+    {
+      cardImage: 'assets/img/testimonials/testimonial_a.svg',
+      hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
+      name: 'Weihnachtsmann',
+      commentKey: 'home.testimonials.comment_three',
+      profileUrl: 'https://de.linkedin.com',
+      rotationClass: 'rotate_right'
+    }
+  ];
 }
