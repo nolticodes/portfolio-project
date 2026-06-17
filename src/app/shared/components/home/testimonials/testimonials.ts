d74d82f63dcb302e1e6c63453a5676c861443982
Denis Nolting <denis.nolting@gmx.de>
2026-06-17 18:28:22 +0200

    refactor: render testimonials from data array with @for loop

--- src/app/shared/components/home/testimonials/testimonials.ts
+++ src/app/shared/components/home/testimonials/testimonials.ts
@@ -7,4 +7,29 @@ import { TranslatePipe } from '@ngx-translate/core';
   templateUrl: './testimonials.html',
   styleUrl: './testimonials.scss',
 })
-export class Testimonials {}
+export class Testimonials {
+  testimonials = [
+  {
+    cardImage: 'assets/img/testimonials/testimonial_a.svg',
+    hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
+    name: 'Weihnachtsmann',
+    commentKey: 'home.testimonials.comment_one',
+    rotationClass: 'rotate_left'
+  },
+  {
+    cardImage: 'assets/img/testimonials/testimonial_b.svg',
+    hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
+    name: 'Osterhase',
+    commentKey: 'home.testimonials.comment_two',
+    rotationClass: ''
+  },
+  {
+    cardImage: 'assets/img/testimonials/testimonial_a.svg',
+    hoverImage: 'assets/img/testimonials/testimonial_hover.svg',
+    name: 'Zahnfee',
+    commentKey: 'home.testimonials.comment_three',
+    profileUrl: 'https://linkedin.com/in/...',
+    rotationClass: 'rotate_right'
+  }
+];
+}
