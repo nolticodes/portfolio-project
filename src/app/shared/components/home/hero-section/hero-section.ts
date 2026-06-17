0f950e11480afba4c87bd8ab14d0966125a479c5
Denis Nolting <denis.nolting@gmx.de>
2026-06-17 17:18:57 +0200

    feat:  added translation pipe for hero section

--- src/app/shared/components/home/hero-section/hero-section.ts
+++ src/app/shared/components/home/hero-section/hero-section.ts
@@ -1,49 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ChangeLetterHover } from '../../../directives/change-letter-hover';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [ChangeLetterHover],
+  imports: [ChangeLetterHover, TranslatePipe],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
 
 export class HeroSection {
-  displayText = 'Hallo Welt';
-  defaultText = 'Hallo Welt';
-  hoverText = 'Ich bin Denis Nolting';
+  private translate = inject(TranslateService);
 
-  private nextText = this.defaultText;
+  displayText = '';
+  defaultText = '';
+  hoverText = '';
+  developerText = '';
+
+  private nextText = '';
+
   isHovered = false;
   isTextFading = false;
   isTextShrinking = false;
   isHandLeaving = false;
 
-onMouseEnter() {
-  this.isHovered = true;
-  this.isHandLeaving = false;
-  this.isTextShrinking = false;
-  this.changeHeroText(this.hoverText);
-}
+  constructor() {
+    this.setHeroTexts();
 
-onMouseLeave() {
-  this.isHovered = false;
-  this.isHandLeaving = true;
-  this.isTextShrinking = true;
-  this.changeHeroText(this.defaultText);
-}
-
-onTextAnimationEnd(event: AnimationEvent) {
-  if (event.animationName === 'wobbleShrink') {
-    this.isTextShrinking = false;
+    this.translate.onLangChange.subscribe(() => {
+      this.setHeroTexts();
+    });
   }
-}
 
-onHandAnimationEnd() {
-  if (this.isHandLeaving) {
+  private setHeroTexts() {
+    this.defaultText = this.translate.instant('home.hero.helloWorld');
+    this.hoverText = this.translate.instant('home.hero.iBims');
+
+    this.displayText = this.isHovered ? this.hoverText : this.defaultText;
+    this.nextText = this.displayText;
+  }
+
+  onMouseEnter() {
+    this.isHovered = true;
     this.isHandLeaving = false;
+    this.isTextShrinking = false;
+    this.changeHeroText(this.hoverText);
   }
-}
+
+  onMouseLeave() {
+    this.isHovered = false;
+    this.isHandLeaving = true;
+    this.isTextShrinking = true;
+    this.changeHeroText(this.defaultText);
+  }
 
   changeHeroText(newText: string) {
     if (newText === this.displayText) return;
@@ -58,5 +67,17 @@ onHandAnimationEnd() {
 
     this.displayText = this.nextText;
     this.isTextFading = false;
+  }
+
+  onTextAnimationEnd(event: AnimationEvent) {
+    if (event.animationName === 'wobbleShrink') {
+      this.isTextShrinking = false;
+    }
+  }
+
+  onHandAnimationEnd() {
+    if (this.isHandLeaving) {
+      this.isHandLeaving = false;
+    }
   }
 }
