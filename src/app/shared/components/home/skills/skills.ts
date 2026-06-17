fdbf9b8e39c5fac8475991762d21d3f5d39fe0fd
Denis Nolting <denis.nolting@gmx.de>
2026-06-17 17:51:08 +0200

    feat: add transaltion pipe for skills section

--- src/app/shared/components/home/skills/skills.ts
+++ src/app/shared/components/home/skills/skills.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
