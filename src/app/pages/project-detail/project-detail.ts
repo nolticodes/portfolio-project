da6f4e705091657f02c1fff0f83279214e15a642
Denis Nolting <denis.nolting@gmx.de>
2026-06-19 10:28:23 +0200

    style and feat: styling and go to next project method

--- src/app/pages/project-detail/project-detail.ts
+++ src/app/pages/project-detail/project-detail.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-project-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './project-detail.html',
   styleUrl: './project-detail.scss',
 })
 export class ProjectDetail {
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   projects = [
     {
@@ -17,7 +18,20 @@ export class ProjectDetail {
       description: 'Task management app',
       implemantationDetails: '...',
       imageUrl: '/assets/img/projects/Laptop.svg',
-      technologies: ['Angular', 'TypeScript', 'SCSS'],
+      technologies: [
+        {
+          name: 'Angular',
+          imageUrl: '',
+        },
+        {
+          name: 'TypeScript',
+          imageUrl: '/assets/icons/frontend_skills/Ts.svg',
+        },
+        {
+          name: 'SCSS',
+          imageUrl: '/assets/icons/frontend_skills/CSS.svg',
+        },
+      ],
       duration: '3 Weeks',
       linkGithub: '',
       linkLiveTest: '',
@@ -28,7 +42,20 @@ export class ProjectDetail {
       description: 'Jump and run game',
       implemantationDetails: '...',
       imageUrl: '/assets/img/projects/Pollo.svg',
-      technologies: ['JavaScript', 'HTML', 'CSS'],
+      technologies: [
+        {
+          name: 'Angular',
+          imageUrl: '/assets/icons/frontend_skills/Angular.svg',
+        },
+        {
+          name: 'TypeScript',
+          imageUrl: '/assets/icons/frontend_skills/Ts.svg',
+        },
+        {
+          name: 'SCSS',
+          imageUrl: '/assets/icons/frontend_skills/CSS.svg',
+        },
+      ],
       duration: '3 Weeks',
       linkGithub: '',
       linkLiveTest: '',
@@ -39,14 +66,47 @@ export class ProjectDetail {
       description: 'Jump and run game',
       implemantationDetails: '...',
       imageUrl: '/assets/img/projects/Pollo.svg',
-      technologies: ['JavaScript', 'HTML', 'CSS'],
+      technologies: [
+        {
+          name: 'Angular',
+          imageUrl: '',
+        },
+        {
+          name: 'TypeScript',
+          imageUrl: '/assets/icons/frontend_skills/Ts.svg',
+        },
+        {
+          name: 'SCSS',
+          imageUrl: '/assets/icons/frontend_skills/CSS.svg',
+        },
+      ],
       duration: '3 Weeks',
       linkGithub: '',
       linkLiveTest: '',
     }
   ];
+  
+  project = this.projects[0];
 
-  project = this.projects.find(project =>
-    project.slug === this.route.snapshot.paramMap.get('slug')
-  );
+  constructor() {
+    this.route.paramMap.subscribe(params => {
+      const slug = params.get('slug');
+      const foundProject = this.projects.find(project => project.slug === slug);
+
+      if (foundProject) {
+        this.project = foundProject;
+      }
+    });
+  }
+
+  goToNextProject() {
+    const currentIndex = this.projects.findIndex(
+      project => project.slug === this.project.slug
+    );
+    const nextIndex = (currentIndex + 1) % this.projects.length;
+    const nextProject = this.projects[nextIndex];
+
+    this.router.navigate(['/projects', nextProject.slug]);
+  }
+
 }
