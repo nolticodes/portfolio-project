f325153ed5028f4edc6b8e0c3c41977daf2d4b1a
Denis Nolting <denis.nolting@gmx.de>
2026-06-23 16:30:30 +0200

    style: contact component

--- src/app/shared/components/home/contact/contact-form/contact-form.ts
+++ src/app/shared/components/home/contact/contact-form/contact-form.ts
@@ -1,31 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 
-export function forbiddenNameValidator(): ValidatorFn {
+export function forbiddenEmailValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const eMailValue = control.value;
     const hasValidEnding = eMailValue.endsWith('.com') || eMailValue.endsWith('.de');
-    return !hasValidEnding ? {invalidEmailEnding: {value: eMailValue}} : null;
+    return !hasValidEnding ? { invalidEmailEnding: { forbiddenEmail: eMailValue } } : null;
   };
 }
 
+export function forbiddenEmailValidatorTwo(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    return control.value == "Your name goes here" ? { forbiddenEmail: { value: control.value } } : null;
+  };
+}
+
+export function forbiddenNameValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    return control.value == "youremail@email.com" ? { forbiddenName: { value: control.value } } : null;
+  };
+}
+
+export function forbiddenMessageValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    return control.value == "Hello Denis, I am interested in ..." ? { forbiddenMessage: { value: control.value } } : null;
+  };
+}
+
+
 @Component({
   selector: 'app-contact-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, TranslatePipe],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
 
+  private translate = inject(TranslateService);
+
   userForm = new FormGroup({
     firstName: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(4)]
+      validators: [Validators.required, Validators.minLength(4), forbiddenNameValidator()]
     }),
     eMail: new FormControl('', {
-      validators: [Validators.required, Validators.email]
+      validators: [Validators.required, Validators.email, forbiddenEmailValidator(), forbiddenEmailValidatorTwo()]
     }),
+    message: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(10), Validators.maxLength(200), forbiddenMessageValidator()]
+    }),
+    privacyPolicy: new FormControl(false, {
+      validators: [Validators.requiredTrue]
+    })
   })
 
   formSubmit() {
@@ -33,7 +61,12 @@ export class ContactForm {
   }
 
   formReset() {
-    this.userForm.reset
+    this.userForm.reset({
+      firstName: '',
+      eMail: '',
+      message: '',
+      privacyPolicy: false
+    });
   }
 
   get email() {
--- src/app/shared/components/home/contact/contact.ts
+++ src/app/shared/components/home/contact/contact.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ContactForm } from './contact-form/contact-form';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [ContactForm],
+  imports: [ContactForm, TranslatePipe],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
