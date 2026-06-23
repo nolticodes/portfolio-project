a76b80d92ff28d5ca71958dce0be429fed7fc5a1
Denis Nolting <denis.nolting@gmx.de>
2026-06-23 09:13:45 +0200

    feat: added contact component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,10 +4,11 @@ import { AboutMe } from '../../shared/components/home/about-me/about-me';
 import { Skills } from '../../shared/components/home/skills/skills';
 import { Testimonials } from '../../shared/components/home/testimonials/testimonials';
 import { Projects } from "../../shared/components/home/projects/projects";
+import { Contact } from '../../shared/components/home/contact/contact';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, AboutMe, Skills, Testimonials, Projects],
+  imports: [HeroSection, AboutMe, Skills, Testimonials, Projects, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
