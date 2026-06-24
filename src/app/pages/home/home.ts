824814709d7f4337fc09377c210b3d0d938eb51b
Denis Nolting <denis.nolting@gmx.de>
2026-06-24 22:50:51 +0200

    feat: add new hero section toreplace old hero

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,10 +5,11 @@ import { Skills } from '../../shared/components/home/skills/skills';
 import { Testimonials } from '../../shared/components/home/testimonials/testimonials';
 import { Projects } from "../../shared/components/home/projects/projects";
 import { Contact } from '../../shared/components/home/contact/contact';
+import { Hero } from "../../shared/components/home/hero/hero";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, AboutMe, Skills, Testimonials, Projects, Contact],
+  imports: [HeroSection, AboutMe, Skills, Testimonials, Projects, Contact, Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- src/app/shared/components/home/hero/hero.ts
+++ src/app/shared/components/home/hero/hero.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { ChangeLetterHover } from '../../../directives/change-letter-hover';
 
 @Component({
   selector: 'app-hero',
-  imports: [TranslatePipe],
+  imports: [ChangeLetterHover, TranslatePipe],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+
+}
