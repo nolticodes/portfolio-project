3603cb9c3510d5808494aff9961a5b1cc26924de
Denis Nolting <denis.nolting@gmx.de>
2026-06-25 19:23:32 +0200

    feat: different svg in footer different roots

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -27,4 +27,22 @@ export class Footer {
       '/privacy-policy'
     ].includes(this.router.url);
   }
+
+  get linkedinIcon() {
+    return this.isStaticPage
+      ? '/assets/icons/contact_icons/linkedin_black.svg'
+      : '/assets/icons/contact_icons/linkedin.svg';
+  }
+
+  get githubIcon() {
+    return this.isStaticPage
+      ? '/assets/icons/contact_icons/github_black.svg'
+      : '/assets/icons/contact_icons/github.svg';
+  }
+
+  get mailIcon() {
+    return this.isStaticPage
+      ? '/assets/icons/contact_icons/mail_black.svg'
+      : '/assets/icons/contact_icons/mail.svg';
+  }
 }
