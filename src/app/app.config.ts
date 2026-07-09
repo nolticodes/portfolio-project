129a56fe9ebabca11170fe1b848b9df11807dac7
Denis Nolting <denis.nolting@gmx.de>
2026-07-09 10:22:03 +0200

    refactor: href links to routerlinks with fragment ID

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,30 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
-import {provideHttpClient} from "@angular/common/http";
-import {provideTranslateService, TranslateService} from "@ngx-translate/core";
-import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
+import { provideHttpClient } from "@angular/common/http";
+import { provideTranslateService, TranslateService } from "@ngx-translate/core";
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      })
+    ),
     provideHttpClient(),
     provideTranslateService({
       lang: 'en',
       fallbackLang: 'en',
       loader: provideTranslateHttpLoader({
         prefix: './i18n/',
-        suffix: '.json'
-      })
+        suffix: '.json',
+      }),
     }),
-  ]
+  ],
 };
