bf6a6bdf9f599eca1c9d47fb7a4e51f333ac3e18
Denis Nolting <denis.nolting@gmx.de>
2026-08-03 17:14:15 +0200

    rafctor: change href link to routerlink

--- src/app/shared/components/home/hero/hero.ts
+++ src/app/shared/components/home/hero/hero.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ChangeLetterHover } from '../../../directives/change-letter-hover';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [ChangeLetterHover, TranslatePipe],
+  imports: [ChangeLetterHover, TranslatePipe, RouterLink],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
