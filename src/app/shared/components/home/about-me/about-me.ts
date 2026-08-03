1acfb1240c8eb80af7f2c67c07d6e38786e08d65
Denis Nolting <denis.nolting@gmx.de>
2026-08-03 17:15:42 +0200

    feat: routerlink

--- src/app/shared/components/home/about-me/about-me.ts
+++ src/app/shared/components/home/about-me/about-me.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, RouterLink],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss',
 })
