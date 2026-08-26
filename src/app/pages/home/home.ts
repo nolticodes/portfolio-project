27126e38a647565e56a859f09a5e5428791a840a
Denis Nolting <denis.nolting@gmx.de>
2026-08-26 16:32:16 +0200

    doc: clean code

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HeroSection } from '../../shared/components/home/hero-section/hero-section';
 import { AboutMe } from '../../shared/components/home/about-me/about-me';
 import { Skills } from '../../shared/components/home/skills/skills';
 import { Testimonials } from '../../shared/components/home/testimonials/testimonials';
@@ -9,7 +8,7 @@ import { Hero } from "../../shared/components/home/hero/hero";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, AboutMe, Skills, Testimonials, Projects, Contact, Hero],
+  imports: [AboutMe, Skills, Testimonials, Projects, Contact, Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
